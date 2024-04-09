e82e1f92b8146d3eec1d12ffbe44ad1bf56dc918
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-04-09 13:59:48 +0200

    added getters and setters in Book

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,29 @@
 public class Book {
-    public String title;
-    public String author;
-    int price;
+    private String title;
+    private String author;
+    private int price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Book[] books = {new Book(), new Book()};
-        books[0].title = "The Lord of the Rings. Boxed Set";
-        books[0].author = "J. R. R. Tolkien";
-        books[0].price = 71;
-        books[1].title = "Harry Potter. Boxed Set";
-        books[1].author = "J. K. Rowling";
-        books[1].price = 56;
+        books[0].setTitle("The Lord of the Rings. Boxed Set");
+        books[0].setAuthor("J. R. R. Tolkien");
+        books[0].setPrice(71);
+        books[1].setTitle("Harry Potter. Boxed Set");
+        books[1].setAuthor("J. K. Rowling");
+        books[1].setPrice(56);
 
         System.out.println("Books: ");
         for (int i = 0; i < books.length; i++) {
@@ -28,9 +28,9 @@ public class Main {
         }
     }
     public static void printBookDetails(Book book) {
-        System.out.println("Title: " + book.title + "\n"
-                + "Author: " + book.author + "\n"
-                + "Price: " + book.price);
+        System.out.println("Title: " + book.getTitle() + "\n"
+                + "Author: " + book.getAuthor() + "\n"
+                + "Price: " + book.getPrice());
     }
     public static void printMovieDetails(Movie movie) {
         System.out.println("Title: " + movie.title + "\n"
